653567b39c94153fc2979653767410d25e6272ae
fentas <jan.guth@gmail.com>
2018-07-15 22:02:39 +0200

    :bug: entrypoint

--- nix/ghostnode/entrypoint.sh
+++ nix/ghostnode/entrypoint.sh
@@ -12,13 +12,14 @@ set -e
 : ${RPC_USER:=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)}
 : ${PRC_PASSWORD:=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)}
 : ${RPC_ALLOW_IP:=127.0.0.1}
+: ${EXTERNAL_IP:=$(curl --connect-timeout 2 -s4 https://icanhazip.com)}
 
 if [ "${1:0:1}" = '-' ]; then
 	set -- "${DEAMON_BIN}" "$@"
 fi
 
-if [ -d "/.${PROJECT}" ]; then
-  ln -s "/.${PROJECT}" "~/.${PROJECT}"
+if [ -d "/.${PROJECT}" ] && [ ! -d "/root/.${PROJECT}" ]; then
+  ln -s "/.${PROJECT}" "/root/.${PROJECT}"
 fi
 
 if [ "$1" = "${DEAMON_BIN}" ]; then
@@ -29,12 +30,16 @@ if [ "$1" = "${DEAMON_BIN}" ]; then
     exit 1
   fi
 
-  if [ -f /bootstrap.sh ]; then
-    /bootstrap.sh
-  fi
+  if [ ! -f /root/.${PROJECT}/${PROJECT}.conf ]; then
+    if [ -f /bootstrap.sh ]; then
+      /bootstrap.sh
+    fi
 
-  ${DEAMON_BIN} -daemon
-cat <<EOF > ~/.${PROJECT}/${PROJECT}.conf
+    ${DEAMON_BIN} -daemon
+    until ${CLI_BIN} getnetworkinfo 2>/dev/null; do sleep 1; done
+    PRIVKEY="${PRIVKEY:-$(${CLI_BIN} ${MN_NAME} genkey)}"
+    ${CLI_BIN} stop
+cat <<EOF > /root/.${PROJECT}/${PROJECT}.conf
 rpcuser=${RPC_USER}
 rpcpassword=${PRC_PASSWORD}
 rpcallowip=${RPC_ALLOW_IP}
@@ -42,18 +47,18 @@ rpcport=${RPC_PORT}
 port=${MN_PORT}
 listen=1
 server=1
-daemon=1
+daemon=0
 logtimestamps=1
 maxconnections=256
 txindex=1
 ${MN_NAME}=1
-externalip=$(dig +short myip.opendns.com @resolver1.opendns.com):${MN_PORT}
-${MN_NAME}privkey=${PRIVKEY:=$(${CLI_BIN} ${MN_NAME} genkey)}
+externalip=${EXTERNAL_IP}:${MN_PORT}
+${MN_NAME}privkey=${PRIVKEY}
 EOF
-  ${CLI_BIN} stop
+  fi
 
   if [ "$#" -eq 0 ]; then
-    set -- "${DEAMON_BIN}" "-daemon" "-conf=${CONFIG_FOLDER}/${CONFIG_FILE}" "-datadir=${CONFIG_FOLDER}"
+    set -- "${DEAMON_BIN}" "-conf=${CONFIG_FOLDER}/${CONFIG_FILE}" "-datadir=${CONFIG_FOLDER}"
   fi
 fi
 
